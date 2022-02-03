a84a323c8fd8f43651d3f83a86b8b0b037e10a03
William Bergekrans <wilbe251@student.liu.se>
2022-02-03 22:17:55 +0100

    vue app up and running!

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app id="app">
+    <h1>HELLO WORLD</h1>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
