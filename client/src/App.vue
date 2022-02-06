6e05c5e20333a0ed12a149e48c9ff56d6e3bb0aa
William Bergekrans <wilbe251@student.liu.se>
2022-02-06 20:03:45 +0100

    Router setup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,42 @@
 <template>
-  <v-app id="app">
-    <h1>HELLO WORLD</h1>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <Navbar />
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
+
 export default {
-  name: "App",
+  components: {
+    Navbar,
+  },
 };
 </script>
 
+
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+  background-color: lightgreen;
+}
+
+#nav a {
+  font-weight: bold;
+  color: gray;
+}
+
+#nav a.router-link-exact-active {
+  color: black;
 }
 </style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>HELLO ROUTER</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//  import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+};
+</script>
