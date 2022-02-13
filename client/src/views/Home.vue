b134d9fa56765b0e6563211054237d0c21cf2df4
William Bergekrans <wilbe251@student.liu.se>
2022-02-13 23:38:30 +0100

    Props to recipe cards

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,21 @@
 <template>
   <v-container>
     <v-row>
-      <RecipeCard class="ma-4" />
-      <RecipeCard class="ma-4" />
-      <RecipeCard class="ma-4" />
+      <RecipeCard
+        name="Spagetti Bolognese"
+        description="En pasta med krämig köttfärsås"
+        class="ma-4"
+      />
+      <RecipeCard
+        name="Vegetasik lasagne"
+        description="Pasta i flera lager med grönsaker och keso"
+        class="ma-4"
+      />
+      <RecipeCard
+        name="Pannkakor"
+        description="MMMMM ägg, mjölk och sylt etc"
+        class="ma-4"
+      />
     </v-row>
     <v-row justify="center" class="mt-5">
       <v-btn class="mx-2">Slumpa recept</v-btn>
