015b24583de7c0e81b63bb96db749db46315fbed
William Bergekrans <wilbe251@student.liu.se>
2022-02-13 23:13:16 +0100

    State added and design on homepage

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,8 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    randomRecipes: [{}],
+  },
   mutations: {},
   actions: {},
-  modules: {},
+  getters: {
+    getRandomRecipes: (state) => {
+      return state.randomRecipes;
+    },
+  },
 });
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <v-container>
     <v-row>
-      <v-col> <RecipeCard /> </v-col>
+      <RecipeCard class="ma-4" />
+      <RecipeCard class="ma-4" />
+      <RecipeCard class="ma-4" />
+    </v-row>
+    <v-row justify="center" class="mt-5">
+      <v-btn class="mx-2">Slumpa recept</v-btn>
+      <v-btn class="mx-2">Nytt recept</v-btn>
     </v-row>
   </v-container>
 </template>
 
 <script>
 import RecipeCard from "../components/RecipeCard.vue";
+import { mapGetters } from "vuex";
 
 export default {
   name: "Home",
+  data: () => ({
+    recipes: {
+      title: "",
+      description: "",
+      id: NULL,
+    },
+  }),
   components: {
     RecipeCard,
   },
+  computed: { ...mapGetters(["getRandomRecipes"]) },
 };
 </script>
